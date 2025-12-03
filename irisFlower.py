1bd23bd66a136f0d510d7e164ac134b17ffc78ee
Prabesh Bhujel <prabeshbhujel02@gmail.com>
2025-12-03 23:51:54 +0545

    A

--- irisFlower.py
+++ irisFlower.py
@@ -22,3 +22,21 @@ print(df.describe())
 #visualize relationship
 sns.pairplot(df,hue="Species")
 plt.show()
+
+#train, test, split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=42)
+
+#Feature Scaling
+
+#Train Logistic Regression Model
+model = LogisticRegression(max_iter=200)
+model.fit(x_train,y_train)
+
+#prediction
+y_pred = model.predict(x_test)
+
+#Evaluate Model
+print("Accuracy:",accuracy_score(y_test,y_pred))
+
+print("Confusion Matrix:",confusion_matrix(y_test,y_pred))
+print("Classification Report:", classification_report(y_test,y_pred))
